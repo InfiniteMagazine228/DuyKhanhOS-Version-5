3ac9307973c174be8b97e1866aa4c9c0260b2480
InfiniteMagazine228 <haduykhanhpham@gmail.com>
2026-06-21 21:22:16 +0700

    Update grub.sh

--- scripts/grub.sh
+++ scripts/grub.sh
@@ -2,25 +2,26 @@
 set -eux
 
 mkdir -p iso/boot/grub
+mkdir -p iso/boot/grub/i386-pc
 mkdir -p iso/EFI/boot
 
-cp config/grub/grub.cfg iso/boot/grub/grub.cfg
+cp config/grub/grub.cfg iso/boot/grub/
 
-# UEFI
+# ===== UEFI =====
+
 grub-mkstandalone \
   -O x86_64-efi \
   -o iso/EFI/boot/bootx64.efi \
   "boot/grub/grub.cfg=iso/boot/grub/grub.cfg"
 
-# BIOS modules
-mkdir -p iso/boot/grub/i386-pc
+# ===== Legacy BIOS =====
 
 grub-mkimage \
   -O i386-pc \
   -o iso/boot/grub/core.img \
   -p /boot/grub \
-  biosdisk iso9660 normal linux search configfile \
-  part_gpt part_msdos ext2 fat
+  biosdisk iso9660 normal linux configfile \
+  search part_gpt part_msdos ext2 fat
 
 cat \
 /usr/lib/grub/i386-pc/cdboot.img \
