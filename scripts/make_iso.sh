0afb8442c3ea37c89cd2a28ddfa004bd61eb2d6a
InfiniteMagazine228 <haduykhanhpham@gmail.com>
2026-06-21 21:09:21 +0700

    Update make_iso.sh

--- scripts/make_iso.sh
+++ scripts/make_iso.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+set -eux
 
 mkdir -p output
 
 xorriso -as mkisofs \
 -r \
--V DuyKhanhOS \
--o output/DuyKhanhOS-5.0-final-amd64.iso \
 -J \
 -joliet-long \
 -l \
 -iso-level 3 \
+-V "DuyKhanhOS" \
+-o output/DuyKhanhOS-5.0-final-amd64.iso \
 -eltorito-alt-boot \
 -e EFI/boot/bootx64.efi \
 -no-emul-boot \
