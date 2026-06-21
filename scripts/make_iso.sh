9191870e939bc56b3f0f914c079a428b6c0a4576
InfiniteMagazine228 <haduykhanhpham@gmail.com>
2026-06-21 22:42:19 +0700

    Update make_iso.sh

--- scripts/make_iso.sh
+++ scripts/make_iso.sh
@@ -3,21 +3,6 @@ set -eux
 
 mkdir -p output
 
-xorriso -as mkisofs \
--r \
--V "DuyKhanhOS" \
--o output/DuyKhanhOS-5.0-final-amd64.iso \
--J \
--joliet-long \
--l \
--iso-level 3 \
--b boot/grub/bios.img \
--no-emul-boot \
--boot-load-size 4 \
--boot-info-table \
--eltorito-alt-boot \
--e EFI/boot/bootx64.efi \
--no-emul-boot \
-iso
-
-echo "ISO created."
+grub-mkrescue \
+    -o output/DuyKhanhOS-5.0-final-amd64.iso \
+    iso
