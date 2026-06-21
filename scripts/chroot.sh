3e2e7f16d8ff9ac9e8c84b63c6cef6d608a7b36f
InfiniteMagazine228 <haduykhanhpham@gmail.com>
2026-06-21 19:27:11 +0700

    Update chroot.sh

--- scripts/chroot.sh
+++ scripts/chroot.sh
@@ -1,11 +1,38 @@
 #!/bin/bash
+set -eux
 
 apt update
 
 apt install -y \
-$(cat /root/core.list) \
-$(cat /root/dev.list) \
-$(cat /root/desktop.list) \
-$(cat /root/live.list)
+linux-image-amd64 \
+live-boot \
+live-config \
+systemd-sysv \
+sudo \
+network-manager \
+bash \
+zsh \
+curl \
+wget \
+git \
+nano \
+vim \
+htop \
+xfce4 \
+xfce4-goodies \
+xfce4-terminal \
+thunar \
+lightdm \
+lightdm-gtk-greeter \
+firefox-esr \
+vlc \
+gparted \
+file-roller
+
+useradd -m -s /bin/bash live || true
+
+echo "live:live" | chpasswd
+
+usermod -aG sudo live
 
 apt clean
