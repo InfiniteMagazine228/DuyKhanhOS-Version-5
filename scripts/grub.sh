1b4069a23ec860a639ea727ed0488a247d39bae9
InfiniteMagazine228 <haduykhanhpham@gmail.com>
2026-06-21 21:33:13 +0700

    Update grub.sh

--- scripts/grub.sh
+++ scripts/grub.sh
@@ -1,35 +1,49 @@
 #!/bin/bash
 set -eux
 
+# ===== Directories =====
+
 mkdir -p iso/boot/grub
 mkdir -p iso/boot/grub/themes/DuyKhanhOS
 mkdir -p iso/boot/grub/fonts
-mkdir -p iso/boot/grub/i386-pc
 mkdir -p iso/EFI/boot
 
-cp config/grub/grub.cfg iso/boot/grub/
+# ===== Config =====
 
-cp -r config/grub/themes/DuyKhanhOS/* \
-iso/boot/grub/themes/DuyKhanhOS/
+cp config/grub/grub.cfg iso/boot/grub/grub.cfg
+
+# ===== Theme =====
+
+if [ -d config/grub/themes/DuyKhanhOS ]; then
+    cp -r config/grub/themes/DuyKhanhOS/* \
+        iso/boot/grub/themes/DuyKhanhOS/
+fi
+
+# ===== Font =====
 
 cp /usr/share/grub/unicode.pf2 \
-iso/boot/grub/fonts/
+   iso/boot/grub/fonts/
 
-# UEFI
+# ===== UEFI =====
+
 grub-mkstandalone \
--O x86_64-efi \
--o iso/EFI/boot/bootx64.efi \
-"boot/grub/grub.cfg=iso/boot/grub/grub.cfg"
+  -O x86_64-efi \
+  -o iso/EFI/boot/bootx64.efi \
+  --modules="part_gpt part_msdos fat ext2 iso9660 normal linux configfile search gfxterm png all_video" \
+  "boot/grub/grub.cfg=iso/boot/grub/grub.cfg"
 
-# BIOS
+# ===== Legacy BIOS =====
+
 grub-mkimage \
--O i386-pc \
--o iso/boot/grub/core.img \
--p /boot/grub \
-biosdisk iso9660 normal linux configfile \
-search part_gpt part_msdos ext2 fat
+  -O i386-pc \
+  -o iso/boot/grub/core.img \
+  -p /boot/grub \
+  biosdisk iso9660 normal linux configfile \
+  search part_gpt part_msdos ext2 fat
 
 cat \
 /usr/lib/grub/i386-pc/cdboot.img \
 iso/boot/grub/core.img \
 > iso/boot/grub/bios.img
+
+echo "GRUB done."
