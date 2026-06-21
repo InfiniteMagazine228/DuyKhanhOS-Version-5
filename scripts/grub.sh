45f92fbe0306a7184d8de001b26a5600e74ceb74
InfiniteMagazine228 <haduykhanhpham@gmail.com>
2026-06-21 21:30:17 +0700

    Update grub.sh

--- scripts/grub.sh
+++ scripts/grub.sh
@@ -2,26 +2,32 @@
 set -eux
 
 mkdir -p iso/boot/grub
+mkdir -p iso/boot/grub/themes/DuyKhanhOS
+mkdir -p iso/boot/grub/fonts
 mkdir -p iso/boot/grub/i386-pc
 mkdir -p iso/EFI/boot
 
 cp config/grub/grub.cfg iso/boot/grub/
 
-# ===== UEFI =====
+cp -r config/grub/themes/DuyKhanhOS/* \
+iso/boot/grub/themes/DuyKhanhOS/
 
+cp /usr/share/grub/unicode.pf2 \
+iso/boot/grub/fonts/
+
+# UEFI
 grub-mkstandalone \
-  -O x86_64-efi \
-  -o iso/EFI/boot/bootx64.efi \
-  "boot/grub/grub.cfg=iso/boot/grub/grub.cfg"
+-O x86_64-efi \
+-o iso/EFI/boot/bootx64.efi \
+"boot/grub/grub.cfg=iso/boot/grub/grub.cfg"
 
-# ===== Legacy BIOS =====
-
+# BIOS
 grub-mkimage \
-  -O i386-pc \
-  -o iso/boot/grub/core.img \
-  -p /boot/grub \
-  biosdisk iso9660 normal linux configfile \
-  search part_gpt part_msdos ext2 fat
+-O i386-pc \
+-o iso/boot/grub/core.img \
+-p /boot/grub \
+biosdisk iso9660 normal linux configfile \
+search part_gpt part_msdos ext2 fat
 
 cat \
 /usr/lib/grub/i386-pc/cdboot.img \
