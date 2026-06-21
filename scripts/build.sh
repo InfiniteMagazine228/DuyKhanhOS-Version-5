1ea376abacc2e3b2a65fbe78cc9bdf338c2ccb7c
InfiniteMagazine228 <haduykhanhpham@gmail.com>
2026-06-21 19:27:30 +0700

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-set -e
+set -eux
 
 ROOTFS=rootfs
 
+sudo rm -rf "$ROOTFS"
+
 sudo debootstrap \
---arch=amd64 \
-trixie \
-$ROOTFS \
-http://deb.debian.org/debian
+  --arch=amd64 \
+  trixie \
+  "$ROOTFS" \
+  http://deb.debian.org/debian
 
-sudo cp scripts/chroot.sh $ROOTFS/root/
+sudo cp scripts/chroot.sh "$ROOTFS/root/chroot.sh"
 
-sudo chroot $ROOTFS chmod +x /root/chroot.sh
-sudo chroot $ROOTFS /root/chroot.sh
+sudo mount --bind /dev "$ROOTFS/dev"
+sudo mount --bind /proc "$ROOTFS/proc"
+sudo mount --bind /sys "$ROOTFS/sys"
+
+sudo chroot "$ROOTFS" /bin/bash /root/chroot.sh
+
+sudo umount "$ROOTFS/dev"
+sudo umount "$ROOTFS/proc"
+sudo umount "$ROOTFS/sys"
