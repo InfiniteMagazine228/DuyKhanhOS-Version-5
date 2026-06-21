7a76d0c542791e3fd9fadfbdd9476bf94c339529
InfiniteMagazine228 <haduykhanhpham@gmail.com>
2026-06-21 21:08:26 +0700

    Add support for i386-pc in grub.sh

--- scripts/grub.sh
+++ scripts/grub.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -eux
 
 mkdir -p iso/boot/grub
 mkdir -p iso/EFI/boot
@@ -6,6 +7,11 @@ mkdir -p iso/EFI/boot
 cp config/grub/grub.cfg iso/boot/grub/
 
 grub-mkstandalone \
--O x86_64-efi \
--o iso/EFI/boot/bootx64.efi \
-boot/grub/grub.cfg=iso/boot/grub/grub.cfg
+    -O x86_64-efi \
+    -o iso/EFI/boot/bootx64.efi \
+    "boot/grub/grub.cfg=iso/boot/grub/grub.cfg"
+
+grub-mkstandalone \
+    -O i386-pc \
+    -o iso/boot/grub/core.img \
+    "boot/grub/grub.cfg=iso/boot/grub/grub.cfg"
