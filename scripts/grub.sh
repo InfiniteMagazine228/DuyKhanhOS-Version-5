005af6224335339d98e6ef9634a789ebbd75756c
InfiniteMagazine228 <haduykhanhpham@gmail.com>
2026-06-21 21:18:48 +0700

    Update grub.sh

--- scripts/grub.sh
+++ scripts/grub.sh
@@ -4,14 +4,9 @@ set -eux
 mkdir -p iso/boot/grub
 mkdir -p iso/EFI/boot
 
-cp config/grub/grub.cfg iso/boot/grub/
+cp config/grub/grub.cfg iso/boot/grub/grub.cfg
 
 grub-mkstandalone \
-    -O x86_64-efi \
-    -o iso/EFI/boot/bootx64.efi \
-    "boot/grub/grub.cfg=iso/boot/grub/grub.cfg"
-
-grub-mkstandalone \
-    -O i386-pc \
-    -o iso/boot/grub/core.img \
-    "boot/grub/grub.cfg=iso/boot/grub/grub.cfg"
+  -O x86_64-efi \
+  -o iso/EFI/boot/bootx64.efi \
+  "boot/grub/grub.cfg=iso/boot/grub/grub.cfg"
