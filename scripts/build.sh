aef24cc4f5fca874aff54da331f297c20f7e8700
InfiniteMagazine228 <haduykhanhpham@gmail.com>
2026-06-22 18:14:52 +0700

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,19 +6,29 @@ ROOTFS=rootfs
 sudo rm -rf "$ROOTFS"
 
 sudo debootstrap \
-  --arch=amd64 \
-  trixie \
-  "$ROOTFS" \
-  http://deb.debian.org/debian
+    --arch=amd64 \
+    trixie \
+    "$ROOTFS" \
+    http://deb.debian.org/debian
 
 sudo cp scripts/chroot.sh "$ROOTFS/root/chroot.sh"
 
-sudo mount --bind /dev "$ROOTFS/dev"
-sudo mount --bind /proc "$ROOTFS/proc"
-sudo mount --bind /sys "$ROOTFS/sys"
+# DNS
+sudo cp /etc/resolv.conf "$ROOTFS/etc/resolv.conf"
 
+# Mount
+sudo mount --bind /dev "$ROOTFS/dev"
+sudo mount --bind /dev/pts "$ROOTFS/dev/pts"
+sudo mount -t proc proc "$ROOTFS/proc"
+sudo mount -t sysfs sys "$ROOTFS/sys"
+sudo mount -t tmpfs tmpfs "$ROOTFS/run"
+
+# Chroot
 sudo chroot "$ROOTFS" /bin/bash /root/chroot.sh
 
-sudo umount "$ROOTFS/dev"
-sudo umount "$ROOTFS/proc"
-sudo umount "$ROOTFS/sys"
+# Cleanup
+sudo umount -lf "$ROOTFS/dev/pts" || true
+sudo umount -lf "$ROOTFS/dev" || true
+sudo umount -lf "$ROOTFS/proc" || true
+sudo umount -lf "$ROOTFS/sys" || true
+sudo umount -lf "$ROOTFS/run" || true
